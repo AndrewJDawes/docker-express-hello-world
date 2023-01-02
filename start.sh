267bb66cf3b58338e7d2a4e1f7921a1e7feaa79c
Andrew Dawes <andrewjamesdawes@gmail.com>
2023-01-02 15:45:16 -0500

    Move start.sh to run.sh

--- start.sh
+++ start.sh
@@ -2,18 +2,5 @@
 if [ -f "getenv.sh" ]; then
     source getenv.sh
 fi
-
-# Attach to nginx-reverse-proxy network
-network_name="nginx-reverse-proxy"
-network_cmd="docker network inspect $network_name || docker network create $network_name"
-eval "$network_cmd"
-
-eval_cmd="docker run \
--d \
---restart=unless-stopped \
---name $PROJECT_DOCKER_CONTAINER_NAME \
---network=$network_name \
--p 80 \
-$PROJECT_DOCKER_FULL_PATH
-"
+eval_cmd="docker start $PROJECT_DOCKER_CONTAINER_NAME"
 eval "$eval_cmd"
